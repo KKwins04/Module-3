17399a0cd328b167d71c8c4bb1348106b9adf16e
KKwins04 <arvind_kavin_karthik@students.edu.sg>
2025-12-18 16:30:11 +0800

    Continue commit

---
+++ Continue.py
@@ -0,0 +1,9 @@
+var = 10 #initialise
+while var > 0:
+    var = var - 1
+    if var == 2:
+        continue
+        #Display result
+    print("\nCurrent variable value: ", var)
+
+print("\nGoodbye!!")
